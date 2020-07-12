2d568667c2ad1d21a956059c1086fe3c0ac261e6
Romano Giannetti <romano@rgtti.com>
2020-07-12 11:46:53 +0200

    Try to set-up the building environment

---
+++ get_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xv
+#
+sudo apt-get install build-essential
+sudo apt-get install libqt4-dev libqt4-qt3support
+sudo apt-get install automake libtool libtool-bin gperf flex bison
+sudo apt-get install doxygen
+sudo apt-get install octave octave-epstk
+sudo apt-get install latex2html
+sudo apt-get install texlive texlive-font-utils texlive-science-doc texlive-science
+sudo apt-get install texlive-publishers texlive-science
+sudo apt-get install transfig gnuplot graphviz
+sudo apt-get install ps2eps pgf python-tk
+
