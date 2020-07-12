27c7ec45fc78f8d34e34455ef5e41c667995199b
Romano Giannetti <romano@rgtti.com>
2020-07-12 18:19:26 +0200

    Build AppImage for qucs-0.0.20-rc2

--- compile_qucs.sh
+++ compile_qucs.sh
@@ -16,4 +16,6 @@ make install DESTDIR=AppDir
 export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/workdir/qucs-qucs-0.0.20-rc2/build/AppDir/usr/lib
 $GITHUB_WORKSPACE/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 ls -l
-mv ./Qucs-x86_64.AppImage  ./Qucs-0.0.20-pre2-x86_64.AppImage
+cp ./Qucs-*-x86_64.AppImage  $GITHUB_WORKSPACE/Qucs-0.0.20-pre2-x86_64.AppImage
+pwd
+ls -l
